ddd427247589941bc2c33b6217db87c70aa3ffc4
Jose Vega <josevega234@gmail.com>
2022-04-07 01:08:50 +0200

    temporal hack for make a other refactoring
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- Code/ToshibaAir/ToshibaAir/config.h
+++ Code/ToshibaAir/ToshibaAir/config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 
+
 #define MAX_LOG_DATA 72 //store up to 72 readings in a circular buffer
 
 // #define USE_BMP //if BMP sensor installed
@@ -28,3 +29,31 @@ const int DHTPin = D3;
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
 #define RESET_MODE_PIN D4  //button to enter into wifi configuration
+
+#include <Arduino.h>
+#include "helper.hpp"
+#include "LittleFS.h"
+#include "ntp_timer.hpp"
+#include "wifi.hpp"
+#include "ota.hpp"
+#include "sensors.hpp"
+
+
+#define SPIFFS LittleFS //dirty hack not to change names in the migration of SPIFFS to LittleFS
+#ifdef USE_ASYNC //for ESP32
+  #include <ESPAsyncTCP.h_>
+  #include <ESPAsyncWebServer.h_>
+#else //use links2004 websocketserver
+  #include <ESP8266WebServer.h>
+  #include <WebSocketsServer.h>
+#endif
+
+
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <ArduinoJson.h>
+#include <NTPClient.h> 
+#include <WiFiUdp.h>
+#include "toshiba_serial.hpp"
+#include "MySimpleTimer.hpp"
+#include "process_request.hpp"
